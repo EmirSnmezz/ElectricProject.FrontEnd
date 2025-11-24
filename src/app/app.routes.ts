4ffc3652ef65ff15bc01cd726c80b2e86e73a60d
emir <emircan_snmez@outlook.com>
2025-11-25 01:43:58 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page-component/home-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: HomePageComponent },
+    { path:'home', component: HomePageComponent }
+
+];
